186161cc36a3d0b57519347a20d8dc974024d5ac
thienphu.dev97 <thienphu.dev97@gmail.com>
2021-09-23 23:17:07 +0700

    cap nhật layout

---
+++ app/src/main/java/com/example/appcovid/health_fragment.java
@@ -0,0 +1,47 @@
+package com.example.appcovid;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class health_fragment extends Fragment {
+
+    public health_fragment() {
+
+    }
+    public static health_fragment newInstance() {
+        health_fragment fragment = new health_fragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_health_monitoring, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
+
